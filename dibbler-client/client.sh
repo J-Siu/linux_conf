cab457f9779a208a5a69fdd785e1c82034b732c5
John Siu <john.sd.siu@gmail.com>
2020-02-08 20:51:04 -0500

    Initial commit.

---
+++ dibbler-client/client.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+ARGS=$@
+SELF=$0
+DIBBLER_ACTION=$1
+
+function log () {
+	logger $SELF $@
+}
+
+log ENV: `env`
+
+FLAG=1
+IFACE=''
+PREFIX=''
+for i in $DOWNLINK_PREFIXES
+do
+	echo IFACE_NAME: $IFACE_NAME >> $LOG
+	if [[ $FLAG = 1 ]]
+	then
+		IFACE=$i
+	else
+		PREFIX=$i
+		PREFIX_ADD=`echo $PREFIX | cut -d/ -f1`
+		PREFIX_LEN=`echo $PREFIX | cut -d/ -f2`
+
+		log IFACE:$IFACE PREFIX:$PREFIX PREFIX_ADD:$PREFIX_ADD PREFIX_LEN:$PREFIX_LE
+		log `ip address show dev $IFACE`
+	
+		# Flash global ipv6 addresse from iface
+	    	ip -6 addr flush dev $IFACE scope global
+
+		if [ "$DIBBLER_ACTION" == "add" -o "$DIBBLER_ACTION" == "update" ]; then
+			# set the first IP from the prefix
+			CMD="ip address add ${PREFIX_ADD}1/$PREFIX_LEN dev $IFACE"
+			log $CMD
+			$CMD
+		fi
+	fi
+	let FLAG=3-$FLAG
+done
+
